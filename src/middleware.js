05442042713cff2be659794db58927f8cda00550
Michael Maurer <mjmaurer777@gmail.com>
2020-03-25 22:21:30 -0400

    email

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const i18next = require("i18next");
 const Backend = require("i18next-node-fs-backend");
-const { findVolunteerById } = require("./airtable");
+const { findVolunteerByEmail } = require("./airtable");
 
 function addUserInfo(app) {
   return async ({ payload, body, event, context, next }) => {
@@ -20,7 +20,7 @@ function addUserInfo(app) {
       user: userId,
       include_locale: true
     });
-    await findVolunteerById(user.user.profile.email);
+    await findVolunteerByEmail(user.user.profile.email);
     context.userId = userId;
     context.locale = user.user.locale;
     context.userEmail = user.user.profile.email;
