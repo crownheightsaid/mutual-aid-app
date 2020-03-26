cab14248ebc8f11a685ead39430080d028a25165
Michael Maurer <mjmaurer777@gmail.com>
2020-03-26 15:04:11 -0400

    get express isntance

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const { App } = require("@slack/bolt");
+const { App, ExpressReceiver } = require("@slack/bolt");
 const startEvents = require("./src/endpoints/events.js");
 const startInteractivity = require("./src/endpoints/interactivity.js");
 const { initIntl, addUserInfo } = require("./src/middleware.js");
 
-const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN
+const expressReceiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET
 });
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: expressReceiver
+});
+const express = expressReceiver.app;
 
 app.use(addUserInfo(app));
 app.use(initIntl);
@@ -15,12 +19,12 @@ app.error(console.error);
 startEvents(app);
 startInteractivity(app);
 
-app.all("/slacktest", (req, res, next) => {
+express.all("/slacktest", (req, res) => {
   console.log("Accessing the secret section ...\n");
   console.log(req);
   console.log("Response: \n");
   console.log(res);
-  next(); // pass control to the next handler
+  res.status(200).send(); // respond 200 OK to the default health check method
 });
 
 (async () => {
