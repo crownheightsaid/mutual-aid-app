4400327359c3fa61eb08a161984064105d1c8708
Michael Maurer <mjmaurer777@gmail.com>
2020-03-25 22:35:59 -0400

    fix query

--- src/airtable.js
+++ src/airtable.js
@@ -7,7 +7,7 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_KEY }).base(
 exports.findVolunteerByEmail = async email => {
   const record = await base("Volunteers")
     .select({
-      filterByFormula: `{volunteer_email} = '${email}')`
+      filterByFormula: `({volunteer_email} = '${email}')`
     })
     .firstPage(function(err, records) {
       if (err) {
