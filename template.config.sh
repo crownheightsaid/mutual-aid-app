c3adf3970c1827f63d51a3856eed6c8083b99f0f
Alex Quick <alex@alexquick.com>
2020-03-27 16:53:01 -0400

    chore: add new env to template.config.sh

--- template.config.sh
+++ template.config.sh
@@ -6,4 +6,15 @@
 SLACK_BOT_TOKEN= # Under "Install App" (you should install your app in your test workspace)
 SLACK_SIGNING_SECRET= # Under "Basic Information"
 
-heroku config:set SLACK_BOT_TOKEN=$SLACK_BOT_TOKEN SLACK_SIGNING_SECRET=$SLACK_SIGNING_SECRET
+## Airtable
+AIRTABLE_KEY=
+AIRTABLE_BASE=
+
+## Gmail env. See docs/gmail-refresh-token.md
+GOOGLE_CLIENT_KEY=
+GOOGLE_CLIENT_SECRET=
+GOOGLE_REFRESH_TOKEN=
+SYNC_GMAIL=1 # unset or set =0 if you want to disable the in-process gmail sync
+
+## TODO: look into using `declare` or something like `heroku config:set $(xargs <.env)`
+heroku config:set SLACK_BOT_TOKEN=$SLACK_BOT_TOKEN SLACK_SIGNING_SECRET=$SLACK_SIGNING_SECRET GOOGLE_CLIENT_KEY=$GOOGLE_CLIENT_KEY GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET GOOGLE_REFRESH_TOKEN=$GOOGLE_REFRESH_TOKEN SYNC_GMAIL=$SYNC_GMAIL AIRTABLE_KEY=$AIRTABLE_KEY AIRTABLE_BASE=$AIRTABLE_BASE
