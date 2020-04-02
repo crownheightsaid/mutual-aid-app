a14cfd9f88ca66f64fac9a587759f6d394545a70
Michael Maurer <mjmaurer777@gmail.com>
2020-04-02 14:06:44 -0400

    shortcut loggings

--- src/slackapp/endpoints/interactivity.js
+++ src/slackapp/endpoints/interactivity.js
@@ -4,4 +4,12 @@ const slackInteractions = createMessageAdapter(
   process.env.SLACK_SIGNING_SECRET
 );
 
+slackInteractions.action(
+  { type: "message_action", callback_id: "assign-to-delivery" },
+  (payload, respond) => {
+    console.log("Payload");
+    console.log(payload);
+  }
+);
+
 module.exports = slackInteractions.requestListener();
