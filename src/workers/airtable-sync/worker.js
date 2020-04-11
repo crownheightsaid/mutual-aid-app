27a98e05fca358fd02b0a5881543503681d43488
Michael Maurer <mjmaurer777@gmail.com>
2020-04-11 15:27:38 -0400

    feat: move to new separate airtable change detector package

--- src/workers/airtable-sync/worker.js
+++ src/workers/airtable-sync/worker.js
@@ -1,68 +1,54 @@
-const ChangeDetector = require("./ChangeDetector");
+const ChangeDetector = require("airtable-change-detector");
 const updateMessageContent = require("./actions/updateMessageContent");
+const { SENSITIVE_FIELDS, airbase } = require("../../airtable");
 
-const wait = interval => new Promise(r => setTimeout(r, interval));
 const defaultInterval = 5000;
-/**
- * Reusable scheduler for repeating in-process tasks
- * `interval` is between one invocation's end and the next one's start, unlike `setInterval`
- */
-function schedule(taskName, interval, f) {
-  let running = true;
-  (async () => {
-    console.log(`Starting ${taskName} and polling every ${interval}ms`);
-    while (running) {
-      /* eslint-disable */
-      try {
-        await f();
-      } catch (e) {
-        console.error(
-          `Error in ${taskName} poll. Continuing in ${interval}. %O`,
-          e
-        );
-      }
-      await wait(interval);
-    }
-    console.log(`Stopped ${taskName}`);
-  })();
-  return () => {
-    console.log(`Stopping ${taskName}`);
-    running = false;
-  };
-}
 
 function startWorker(interval) {
-  if (interval < defaultInterval) {
+  let pollInterval = interval;
+  if (pollInterval < defaultInterval) {
     console.log(
-      `Interval ${interval} is too low. Clamping to ${defaultInterval}`
+      `Interval ${pollInterval} is too low. Clamping to ${defaultInterval}`
     );
-    interval = defaultInterval;
+    pollInterval = defaultInterval;
   }
-  let requestChanges = new ChangeDetector("Requests", 100);
-  schedule("airtable-sync.requests", interval, async () => {
-    const recordsChanged = await requestChanges.poll();
-    const statusFieldName = "Status";
-    const codeFieldName = "Code";
-    const slackIdFieldName = "Delivery slackid";
-    const triggerBackfillFieldName = "Trigger Backfill";
-    console.info(`Found ${recordsChanged.length} changes in Requests`);
-    for (const record of recordsChanged) {
-      //TODO: use this a few more times from different contexts and think about refactoring the api
-      if (record.didChange(statusFieldName)) {
-        const status = record.get(statusFieldName);
-        const newStatus = record.getPrior(statusFieldName);
-        console.log(
-          `${record.get(codeFieldName)} moved from ${newStatus} -> ${status}`
-        );
-      }
-      if(record.didChange(statusFieldName) || record.didChange(slackIdFieldName)){
-        await updateMessageContent(record)
-      }
-      if(record.didChange(triggerBackfillFieldName)){
-        await updateMessageContent(record)
-      }
-    }
+  const requestChanges = new ChangeDetector(airbase("Requests"), {
+    writeDelayMs: 100,
+    lastProcessedFieldName: "Last Processed",
+    sensitiveFields: SENSITIVE_FIELDS
   });
+  requestChanges.pollWithInterval(
+    "airtable-sync.requests",
+    interval,
+    async recordsChanged => {
+      const statusFieldName = "Status";
+      const codeFieldName = "Code";
+      const slackIdFieldName = "Delivery slackid";
+      const triggerBackfillFieldName = "Trigger Backfill";
+      console.info(`Found ${recordsChanged.length} changes in Requests`);
+      const promises = [];
+      recordsChanged.forEach(record => {
+        // TODO: use this a few more times from different contexts and think about refactoring the api
+        if (record.didChange(statusFieldName)) {
+          const status = record.get(statusFieldName);
+          const newStatus = record.getPrior(statusFieldName);
+          console.log(
+            `${record.get(codeFieldName)} moved from ${newStatus} -> ${status}`
+          );
+        }
+        if (
+          record.didChange(statusFieldName) ||
+          record.didChange(slackIdFieldName)
+        ) {
+          promises.push(updateMessageContent(record));
+        }
+        if (record.didChange(triggerBackfillFieldName)) {
+          promises.push(updateMessageContent(record));
+        }
+      });
+      return Promise.all(promises);
+    }
+  );
 }
 
 module.exports = startWorker;
