fdbbbe06c461d8c95115f238a9e55bca444ac440
Michael Maurer <mjmaurer777@gmail.com>
2020-04-23 01:15:32 -0400

    fix: switch string find args

--- src/lib/airtable/tables/requests.js
+++ src/lib/airtable/tables/requests.js
@@ -89,13 +89,9 @@ exports.findOpenRequestsForSlack = async () => {
 
 exports.findRequestByCode = async code => {
   try {
-    let issue57Code = code || "------";
-    if (issue57Code.length !== 6) {
-      issue57Code = `--${issue57Code}`;
-    }
     const records = await requestsTable
       .select({
-        filterByFormula: `(FIND({${fields.code}}, '${issue57Code}') > 0)`
+        filterByFormula: `(FIND('${code}', {${fields.code}}) > 0)`
       })
       .firstPage();
     if (records.length === 0) {
@@ -134,13 +130,9 @@ exports.findRequestByPhone = async phone => {
 // }
 exports.updateRequestByCode = async (code, update) => {
   try {
-    let issue57Code = code || "------";
-    if (issue57Code.length !== 6) {
-      issue57Code = `--${issue57Code}`;
-    }
     const records = await requestsTable
       .select({
-        filterByFormula: `(FIND({${fields.code}}, '${issue57Code}') > 0)`
+        filterByFormula: `(FIND('${code}', {${fields.code}}) > 0)`
       })
       .firstPage();
     if (records.length === 0) {
