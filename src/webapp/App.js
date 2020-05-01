0f85bc5f07f582cb148252892fcd2c73de3030b3
Sher Minn Chong <sherminn.chong@gmail.com>
2020-05-01 17:44:37 -0400

    Revert master

--- src/webapp/App.js
+++ src/webapp/App.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Router, Redirect } from "@reach/router";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import NeighborhoodFinder from "./pages/NeighborhoodFinder";
-import DeliveryNeeded from "./pages/DeliveryNeeded";
 import "./style/globals.css";
+import "../lib/strings/i18nextInit";
+import DeliveryNeeded from "./pages/DeliveryNeeded";
 
 function App() {
   return (
-    <Router>
-      <Layout path="/">
-        <Redirect from="/en/neighborhood-finder" to="/neighborhood-finder" />
-        <Redirect from="/en/delivery-needed" to="/delivery-needed" />
-        {/* Only components from /pages should be nested here */}
-        <Home path="/" />
-        <NeighborhoodFinder path="/neighborhood-finder" />
-        <DeliveryNeeded path="/delivery-needed" />
-      </Layout>
-    </Router>
+    <Suspense fallback="loading">
+      <Router>
+        <Layout path="/">
+          <Redirect from="/en/neighborhood-finder" to="/neighborhood-finder" />
+          {/* Only components from /pages should be nested here */}
+          <Home path="/" />
+          <NeighborhoodFinder path="/neighborhood-finder" />
+          <DeliveryNeeded path="/delivery-needed" />
+        </Layout>
+      </Router>
+    </Suspense>
   );
 }
 
