d282172a4ef3adf10762d205e91537d8bd219741
Sher Minn Chong <sherminn.chong@gmail.com>
2020-05-01 18:14:13 -0400

    Add new strings

--- src/webapp/pages/DeliveryNeeded.js
+++ src/webapp/pages/DeliveryNeeded.js
@@ -64,31 +64,34 @@ export default function DeliveryNeeded() {
         </Typography>
         <List>
           <ListItem>
-            {str("webapp:deliveryNeeded.description:dot", {
+            {str("webapp:deliveryNeeded.description.dot", {
               defaultValue: `Each dot represents a location with one or more requests. This
             location is only representative of the cross street data. We do not
             store full addresses.`
             })}
           </ListItem>
           <ListItem>
-            {str("webapp:deliveryNeeded.description:clickDot", {defaultValue:`Click on each cluster (large circle with a number) to zoom into
+            {str("webapp:deliveryNeeded.description.clickDot", {
+              defaultValue: `Click on each cluster (large circle with a number) to zoom into
             individual request.`
             })}
           </ListItem>
           <ListItem>
-            {str("webapp:deliveryNeeded.description:popUp", {
+            {str("webapp:deliveryNeeded.description.popUp", {
               defaultValue: `Click on a dot to pop up details. There is a link to the Slack post
             for more details, where you can also claim the delivery.`
             })}
           </ListItem>
           <ListItem>
-            {str("webapp:deliveryNeeded.description:popUp",  {defaultValue:`Some dots may represent multiple requests at the same cross-streets.
+            {str("webapp:deliveryNeeded.description.multipleRequests", {
+              defaultValue: `Some dots may represent multiple requests at the same cross-streets.
             Clicking on them will display all of the requests.`
             })}
           </ListItem>
           <ListItem>
-            {str("webapp:deliveryNeeded.description:questions", {
-              defaultValue: `Questions or concerns? Please let us know in`})}
+            {str("webapp:deliveryNeeded.description.questions", {
+              defaultValue: `Questions or concerns? Please let us know in`
+            })}
             <a href={str("webapp:slack.techChannelUrl")}>
               {str("webapp:slack.techChannel")}
             </a>
