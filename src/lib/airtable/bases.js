0086931664f7c5fecdb079f814895d59949ac8d2
Michael Maurer <mjmaurer777@gmail.com>
2020-05-16 13:41:59 -0400

    fix: don't fail if airbase info isn't set

--- src/lib/airtable/bases.js
+++ src/lib/airtable/bases.js
@@ -1,12 +1,18 @@
 const Airtable = require("airtable");
 
-const airtable = new Airtable({ apiKey: process.env.AIRTABLE_KEY });
-
-exports.paymentsAirbase = process.env.AIRTABLE_PAYMENTS_BASE
-  ? airtable.base(process.env.AIRTABLE_PAYMENTS_BASE)
-  : () => {
-      console.debug("Payments Airbase not registered.");
-    };
-exports.airbase = process.env.AIRTABLE_BASE
-  ? airtable.base(process.env.AIRTABLE_BASE)
+const airtable = process.env.AIRTABLE_KEY
+  ? new Airtable({ apiKey: process.env.AIRTABLE_KEY })
   : null;
+
+exports.paymentsAirbase =
+  airtable && process.env.AIRTABLE_PAYMENTS_BASE
+    ? airtable.base(process.env.AIRTABLE_PAYMENTS_BASE)
+    : () => {
+        console.debug("Payments Airbase not registered.");
+      };
+exports.airbase =
+  airtable && process.env.AIRTABLE_BASE
+    ? airtable.base(process.env.AIRTABLE_BASE)
+    : () => {
+        console.debug("Main Airbase not registered.");
+      };
