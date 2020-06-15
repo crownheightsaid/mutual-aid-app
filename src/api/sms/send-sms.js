a348bcdf5ab7e92b9ffcf0c12f7d4690ec379972
Joey Organisak <jorganisak@gmail.com>
2020-06-14 21:21:47 -0400

    twilio creds and endpoint working

--- src/api/sms/send-sms.js
+++ src/api/sms/send-sms.js
@@ -1,4 +1,23 @@
+const client = require("twilio")(
+  process.env.TWILIO_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
 exports.sendSms = async (req, res) => {
   console.log(req.body);
+  const { phoneNumber, smsMessage } = req.body;
+
+  if (!phoneNumber || !smsMessage) {
+    return res.status(400).send({
+      message: "Expected `phoneNumber` and `message` in payload"
+    });
+  }
+
+  await client.messages.create({
+    body: smsMessage,
+    from: process.env.TWILIO_PHONE_NUMBER,
+    to: phoneNumber
+  });
+
   return res.send({ success: true });
 };
