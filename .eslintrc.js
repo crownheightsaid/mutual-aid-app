292d06806ff467556658e2eb96284ddc89acd39d
Joey Organisak <jorganisak@gmail.com>
2020-06-27 13:31:47 -0400

    ignore node_modules in eslint for new packages in twilio/node_modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,59 @@
 module.exports = {
   root: true,
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   extends: [
-    "airbnb",
-    "react-app",
-    "plugin:no-unsanitized/DOM",
-    "plugin:prettier/recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended"
+    'airbnb',
+    'react-app',
+    'plugin:no-unsanitized/DOM',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
-  plugins: ["react", "prettier", "no-unsanitized", "react-hooks", "jsx-a11y", "promise"],
+  plugins: [
+    'react',
+    'prettier',
+    'no-unsanitized',
+    'react-hooks',
+    'jsx-a11y',
+    'promise',
+  ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
-    sourceType: "module"
+    sourceType: 'module',
   },
+  ignorePatterns: ['node_modules'],
   rules: {
-    "no-unused-vars": ["error", { "varsIgnorePattern": "_.+" }],
-    "no-multi-assign": ["off"],
-    "no-console": ["off"],
-    "react/prop-types": ["off"],
-    "react/static-property-placement": ["error", "static public field"],
-    "react/forbid-prop-types": ["off"],
-    "react/jsx-filename-extension": ["off"],
-    "react/destructuring-assignment": ["off"],
-    "react-hooks/rules-of-hooks": ["error"],
-    "radix": ["off"] //fixed in ES5
+    'no-unused-vars': ['error', {varsIgnorePattern: '_.+'}],
+    'no-multi-assign': ['off'],
+    'no-console': ['off'],
+    'react/prop-types': ['off'],
+    'react/static-property-placement': ['error', 'static public field'],
+    'react/forbid-prop-types': ['off'],
+    'react/jsx-filename-extension': ['off'],
+    'react/destructuring-assignment': ['off'],
+    'react-hooks/rules-of-hooks': ['error'],
+    radix: ['off'], //fixed in ES5
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       alias: {
         map: [
-          ["~lib", "./src/lib"],
-          ["~airtable", "./src/lib/airtable"],
-          ["~slack", "./src/lib/slack"],
-          ["~strings", "./src/lib/strings"],
-          ["~assets", "./src/lib/assets"]
+          ['~lib', './src/lib'],
+          ['~airtable', './src/lib/airtable'],
+          ['~slack', './src/lib/slack'],
+          ['~strings', './src/lib/strings'],
+          ['~assets', './src/lib/assets'],
         ],
-        extensions: ['.ts', '.js', '.jsx', '.json']
+        extensions: ['.ts', '.js', '.jsx', '.json'],
       },
       node: {
-        moduleDirectory: ["node_modules", "src"]
-      }
-    }
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
   },
   globals: {
-    __PATH_PREFIX__: true
-  }
+    __PATH_PREFIX__: true,
+  },
 };
