77eff8ad47d14866c2a9b3305c6cb7cd66e15fee
John Sandfort <jwsandfort@gmail.com>
2020-07-15 10:37:41 -0400

    Add DaysOpenChip component

---
+++ src/webapp/components/DaysOpenChip.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Chip from "@material-ui/core/Chip";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  recent: {
+    backgroundColor: "green"
+  },
+  moderate: {
+    backgroundColor: "orange"
+  },
+  urgent: {
+    backgroundColor: "red"
+  }
+})
+
+export const DaysOpenChip = props => {
+  const classes = useStyles();
+
+  let chipColor;
+
+  if (props.daysOpen < 3) {
+    chipColor = classes.recent;
+  } else if (props.daysOpen < 5) {
+    chipColor = classes.moderate;
+  } else {
+    chipColor = classes.urgent;
+  }
+
+  return (
+    <Chip
+      label={`open for ${props.daysOpen} days`}
+      color="primary"
+      size="small"
+      classes={{
+        colorPrimary: chipColor
+      }}
+    />
+  );
+}
