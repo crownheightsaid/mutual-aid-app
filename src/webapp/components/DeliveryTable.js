033a1406f4b9e51aa8a9a149dc38bfea4d1599ee
Sher Minn Chong <sherminn.chong@gmail.com>
2020-08-09 22:11:02 -0400

    Add scroll to focused row

--- src/webapp/components/DeliveryTable.js
+++ src/webapp/components/DeliveryTable.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext, useEffect } from "react";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -9,49 +9,75 @@ import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import { useTranslation } from "react-i18next";
 import DaysOpenChip from "./DaysOpenChip";
-import { daysSinceSlackMessage } from '../helpers/time';
+import { daysSinceSlackMessage } from "../helpers/time";
 import ClusterMapContext from "../context/ClusterMapContext";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   container: {
-    maxHeight: '90vh',
+    maxHeight: "90vh",
   },
-});
+  focused: {
+    background: theme.palette.grey[100],
+  },
+}));
 
 const DeliveryTable = ({ rows }) => {
   const { t: str } = useTranslation();
   const classes = useStyles();
-  const { focusedRequestId, setFocusedRequestId } = useContext(ClusterMapContext);
+  const { focusedRequestId, setFocusedRequestId } = useContext(
+    ClusterMapContext
+  );
+
+  useEffect(() => {
+    // scroll to focused row
+    const row = document.getElementById(focusedRequestId)
+    const table = document.getElementById('table-wrapper');
+
+    if(row){
+      table.scrollTop = row.offsetTop;
+    }
+  })
 
   // sort happens in-place
   rows.sort((rowA, rowB) => rowA.timestamp - rowB.timestamp);
 
-  const formattedRows = rows;
+  const formattedRows = rows.map((row) => {
+    const isFocused = row.Code === focusedRequestId;
+
+    return {
+      ...row,
+      isFocused,
+    };
+  });
 
   return (
-    <TableContainer className={classes.container} component={Paper}>
-    curr request is {focusedRequestId}
+    <TableContainer id="table-wrapper" className={classes.container} component={Paper}>
+      curr request is{' '}{focusedRequestId}
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>{str("webapp:zoneFinder.label.code")}</TableCell>
             <TableCell>
-              {str("webapp:deliveryNeeded.table.headers.crossStreets", { defaultValue: "Cross streets"})}
+              {str("webapp:deliveryNeeded.table.headers.crossStreets", {
+                defaultValue: "Cross streets",
+              })}
             </TableCell>
+            <TableCell>{str("webapp:zoneFinder.label.firstName")}</TableCell>
+            <TableCell>{str("webapp:zoneFinder.label.slackLink")}</TableCell>
             <TableCell>
-              {str("webapp:zoneFinder.label.firstName")}
-            </TableCell>
-            <TableCell>
-              {str("webapp:zoneFinder.label.slackLink")}
-            </TableCell>
-            <TableCell>
-              {str("webapp:deliveryNeeded.table.headers.daysOpen", { defaultValue: "Days open"})}
+              {str("webapp:deliveryNeeded.table.headers.daysOpen", {
+                defaultValue: "Days open",
+              })}
             </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {formattedRows.map((row) => (
-            <TableRow key={row.Code}>
+            <TableRow
+              id={row.Code}
+              key={row.Code}
+              className={row.isFocused && classes.focused}
+            >
               <TableCell component="th" scope="row">
                 {row.Code}
               </TableCell>
@@ -66,7 +92,12 @@ const DeliveryTable = ({ rows }) => {
                   Slack
                 </a>
               </TableCell>
-              <TableCell><DaysOpenChip timeOnly={true} daysOpen={daysSinceSlackMessage(row.slackTs)} /></TableCell>
+              <TableCell>
+                <DaysOpenChip
+                  timeOnly
+                  daysOpen={daysSinceSlackMessage(row.slackTs)}
+                />
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
