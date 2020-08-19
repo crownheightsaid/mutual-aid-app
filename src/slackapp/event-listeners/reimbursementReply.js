f743bffb7e01330bbc05a91e4a6dedaee525624f
John Sandfort <john@jsand.us>
2020-08-19 17:44:23 -0400

    Test reimbursement reply (#121)
    * Refactor and test
    
    - Extract schemata for donorPayments and paymentRequests
    - Write test for reimbursementReply
    
    * lint
    
    * fix schema imports
    
    * more lint
    
    Co-authored-by: John Sandfort <jwsandfort@gmail.com>

--- src/slackapp/event-listeners/reimbursementReply.js
+++ src/slackapp/event-listeners/reimbursementReply.js
@@ -3,12 +3,13 @@ const { findChannelByName } = require("~slack/channels");
 const { REIMBURSEMENT_CHANNEL } = require("~slack/constants");
 const {
   findPaymentRequestBySlackThreadId,
-  paymentRequestsFields,
 } = require("~airtable/tables/paymentRequests");
 const {
-  createDonorPayment,
-  donorPaymentsFields,
-} = require("~airtable/tables/donorPayments");
+  paymentRequestsFields,
+} = require("~airtable/tables/paymentRequestsSchema");
+const { createDonorPayment } = require("~airtable/tables/donorPayments");
+
+const { donorPaymentsFields } = require("~airtable/tables/donorPaymentsSchema");
 
 module.exports.register = function register(slackEvents) {
   slackEvents.on("message", filterAndReply);
@@ -18,17 +19,31 @@ const filterAndReply = async (event) => {
   if (event.bot_id) {
     return;
   }
-  const reimbursementChannel = await findChannelByName(REIMBURSEMENT_CHANNEL);
-  if (!isReimbursementReply(event, reimbursementChannel.id)) {
+
+  let reimbursementChannel;
+
+  try {
+    reimbursementChannel = await findChannelByName(REIMBURSEMENT_CHANNEL);
+    if (!isReimbursementReply(event, reimbursementChannel.id)) {
+      return;
+    }
+  } catch (e) {
+    console.error("Error finding reimbursement channel", e);
     return;
   }
-  const [paymentRequest] = await findPaymentRequestBySlackThreadId(
-    event.thread_ts
-  );
-  if (!paymentRequest) {
-    console.log("paymentRequest not posted by bot.");
+
+  let paymentRequest;
+  try {
+    [paymentRequest] = await findPaymentRequestBySlackThreadId(event.thread_ts);
+    if (!paymentRequest) {
+      console.log("paymentRequest not posted by bot.");
+      return;
+    }
+  } catch (e) {
+    console.error("Error fetching payment request by Slack thread ID", e);
     return;
   }
+
   const amountMatches = findAmountsInString(event.text);
   if (!amountMatches || amountMatches.length === 0) {
     console.log(`No money matches found in: ${event.text}`);
@@ -37,18 +52,24 @@ const filterAndReply = async (event) => {
 
   const newDonationAmount = Number(amountMatches[0]);
 
-  const [record] = await createDonorPayment({
-    [donorPaymentsFields.amount]: newDonationAmount,
-    [donorPaymentsFields.paymentRequest]: [paymentRequest.getId()],
-    [donorPaymentsFields.status]: donorPaymentsFields.status_options.pending,
-    [donorPaymentsFields.donorSlackId]: event.user,
-    [donorPaymentsFields.recipientConfirmation]:
-      donorPaymentsFields.recipientConfirmation_options.pending,
-    [donorPaymentsFields.donorConfirmation]:
-      donorPaymentsFields.donorConfirmation_options.confirmed,
-  });
-  if (!record) {
-    console.log("Couldn't add donor's payment");
+  try {
+    const [record] = await createDonorPayment({
+      [donorPaymentsFields.amount]: newDonationAmount,
+      [donorPaymentsFields.paymentRequest]: [paymentRequest.getId()],
+      [donorPaymentsFields.status]: donorPaymentsFields.status_options.pending,
+      [donorPaymentsFields.donorSlackId]: event.user,
+      [donorPaymentsFields.recipientConfirmation]:
+        donorPaymentsFields.recipientConfirmation_options.pending,
+      [donorPaymentsFields.donorConfirmation]:
+        donorPaymentsFields.donorConfirmation_options.confirmed,
+    });
+
+    if (!record) {
+      console.log("Couldn't add donor's payment");
+      return;
+    }
+  } catch (e) {
+    console.error("Error adding donor payment", e);
     return;
   }
 
@@ -58,13 +79,18 @@ const filterAndReply = async (event) => {
     newBalance <= 0
       ? "reimbursement is complete!!"
       : `just ${newBalance.toFixed(2)} to go!`;
-  await slackapi.chat.postMessage({
-    token: process.env.SLACK_BOT_TOKEN,
-    channel: reimbursementChannel.id,
-    thread_ts: event.thread_ts,
-    text: `Thanks <@${event.user}>!
-They sent ${newDonationAmount.toFixed(2)}, ${message}`,
-  });
+
+  try {
+    await slackapi.chat.postMessage({
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: reimbursementChannel.id,
+      thread_ts: event.thread_ts,
+      text: `Thanks <@${event.user}>!
+      They sent ${newDonationAmount.toFixed(2)}, ${message}`,
+    });
+  } catch (e) {
+    console.error("Error posting reimbursement message", e);
+  }
 };
 
 const findAmountsInString = (text) => {
@@ -74,3 +100,5 @@ const findAmountsInString = (text) => {
 const isReimbursementReply = (event, reimbursementChannelId) => {
   return event.channel === reimbursementChannelId && event.parent_user_id;
 };
+
+module.exports.filterAndReply = filterAndReply;
---
+++ src/slackapp/event-listeners/reimbursementReply.test.js
@@ -0,0 +1,170 @@
+const { Record } = require("airtable");
+const { filterAndReply } = require("./reimbursementReply");
+
+jest.mock("~slack/webApi", () => ({
+  chat: {
+    postMessage: jest.fn(),
+  },
+}));
+
+const {
+  chat: { postMessage },
+} = require("~slack/webApi");
+
+jest.mock("~airtable/tables/paymentRequests", () => ({
+  findPaymentRequestBySlackThreadId: jest.fn(),
+}));
+const {
+  findPaymentRequestBySlackThreadId,
+} = require("~airtable/tables/paymentRequests");
+const {
+  paymentRequestsTableName,
+  paymentRequestsFields,
+} = require("~airtable/tables/paymentRequestsSchema");
+
+jest.mock("~airtable/tables/donorPayments", () => ({
+  createDonorPayment: jest.fn(),
+}));
+const { createDonorPayment } = require("~airtable/tables/donorPayments");
+
+jest.mock("~slack/channels", () => ({
+  findChannelByName: jest.fn(),
+}));
+const { findChannelByName } = require("~slack/channels");
+
+describe("reimbursement reply", () => {
+  let reimbursementChannelId;
+  let event;
+
+  beforeEach(() => {
+    reimbursementChannelId = "the-reimbursement-channel-id";
+
+    findChannelByName.mockResolvedValue({ id: reimbursementChannelId });
+  });
+
+  afterEach(() => {
+    postMessage.mockClear();
+  });
+
+  describe("when the balance is an even dollar amount", () => {
+    beforeEach(() => {
+      event = {
+        bot_id: null,
+        channel: reimbursementChannelId,
+        parent_user_id: "something",
+        text: "sending 5",
+      };
+    });
+
+    describe("when there is a remaining balance", () => {
+      beforeEach(() => {
+        const paymentRequest = new Record(
+          paymentRequestsTableName,
+          "payment-request-1",
+          {
+            fields: {
+              [paymentRequestsFields.balance]: "6",
+            },
+          }
+        );
+
+        findPaymentRequestBySlackThreadId.mockResolvedValue([paymentRequest]);
+        createDonorPayment.mockResolvedValue([{}]);
+      });
+
+      test("it says how much is left", async () => {
+        await filterAndReply(event);
+
+        expect(postMessage).toHaveBeenCalled();
+        expect(postMessage.mock.calls[0][0].text).toContain("just 1.00 to go");
+      });
+    });
+
+    describe("when the balance is zeroed out", () => {
+      beforeEach(() => {
+        const paymentRequest = new Record(
+          paymentRequestsTableName,
+          "payment-request-2",
+          {
+            fields: {
+              [paymentRequestsFields.balance]: "5",
+            },
+          }
+        );
+
+        findPaymentRequestBySlackThreadId.mockResolvedValue([paymentRequest]);
+        createDonorPayment.mockResolvedValue([{}]);
+      });
+
+      test("it marks the request complete", async () => {
+        await filterAndReply(event);
+
+        expect(postMessage).toHaveBeenCalled();
+        expect(postMessage.mock.calls[0][0].text).toContain(
+          "reimbursement is complete"
+        );
+      });
+    });
+  });
+
+  describe("when the balance has cents", () => {
+    beforeEach(() => {
+      event = {
+        bot_id: null,
+        channel: reimbursementChannelId,
+        parent_user_id: "something",
+        text: "sent $27.15",
+      };
+    });
+
+    describe("when there is a remaining balance", () => {
+      beforeEach(() => {
+        const paymentRequest = new Record(
+          paymentRequestsTableName,
+          "payment-request-1",
+          {
+            fields: {
+              [paymentRequestsFields.balance]: "28.16",
+            },
+          }
+        );
+
+        findPaymentRequestBySlackThreadId.mockResolvedValue([paymentRequest]);
+        createDonorPayment.mockResolvedValue([{}]);
+      });
+
+      test("it says how much is left", async () => {
+        await filterAndReply(event);
+
+        expect(postMessage).toHaveBeenCalled();
+        expect(postMessage.mock.calls[0][0].text).toContain("just 1.01 to go");
+      });
+    });
+
+    describe("when the balance is zeroed out", () => {
+      beforeEach(() => {
+        const paymentRequest = new Record(
+          paymentRequestsTableName,
+          "payment-request-2",
+          {
+            fields: {
+              [paymentRequestsFields.balance]: "27.15",
+            },
+          }
+        );
+
+        findPaymentRequestBySlackThreadId.mockResolvedValue([paymentRequest]);
+        createDonorPayment.mockResolvedValue([{}]);
+      });
+
+      test("it marks the request complete", async () => {
+        await filterAndReply(event);
+
+        expect(postMessage).toHaveBeenCalled();
+        expect(postMessage.mock.calls[0][0].text).toContain(
+          "reimbursement is complete"
+        );
+      });
+    });
+  });
+});
