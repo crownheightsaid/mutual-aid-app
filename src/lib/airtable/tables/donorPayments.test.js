b07d1ccd9df6141aac7672957dde9098605fcaeb
John Sandfort <john@jsand.us>
2020-08-26 12:03:33 -0400

    Test donor payments table (#144)
    * add test for createDonorPayment
    
    * add negative test for createDonorPayment
    
    * add tests for findDonorPaymentByCode
    
    * forgot to lint as usual
    
    * correct test description
    
    Co-authored-by: John Sandfort <jwsandfort@gmail.com>

---
+++ src/lib/airtable/tables/donorPayments.test.js
@@ -0,0 +1,92 @@
+const mockCreate = jest.fn();
+const mockSelect = jest.fn();
+
+jest.mock("~airtable/bases", () => ({
+  paymentsAirbase: () => {
+    return {
+      create: mockCreate,
+      select: mockSelect,
+    };
+  },
+}));
+
+const {
+  createDonorPayment,
+  findDonorPaymentByCode,
+} = require("./donorPayments");
+const { donorPaymentsFields } = require("./donorPaymentsSchema");
+
+const { paymentsAirbase } = require("~airtable/bases");
+
+describe("createDonorPayment", () => {
+  const params = {
+    [donorPaymentsFields.amount]: "some test amount",
+    [donorPaymentsFields.status]: "some test status",
+  };
+
+  describe("on a successful Airtable call", () => {
+    beforeEach(() => {
+      mockCreate.mockResolvedValue("value");
+    });
+
+    test("it creates a donor payment record with the given fields", async () => {
+      const result = await createDonorPayment(params);
+
+      expect(paymentsAirbase().create).toHaveBeenCalledWith([
+        { fields: params },
+      ]);
+      expect(result).toEqual(["value", null]);
+    });
+  });
+
+  describe("on an unsuccessful Airtable call", () => {
+    beforeEach(() => {
+      mockCreate.mockRejectedValue("error");
+    });
+
+    test("it returns an error message", async () => {
+      const result = await createDonorPayment(params);
+
+      expect(paymentsAirbase().create).toHaveBeenCalledWith([
+        { fields: params },
+      ]);
+      expect(result).toEqual([null, "error"]);
+    });
+  });
+});
+
+describe("findDonorPaymentByCode", () => {
+  describe("given a code for an existing donor payment record", () => {
+    beforeEach(() => {
+      mockSelect.mockReturnValue({ firstPage: () => ["some test record"] });
+    });
+
+    it("returns that donor payment record", async () => {
+      const result = await findDonorPaymentByCode("some test code");
+
+      expect(mockSelect).toHaveBeenCalledWith({
+        filterByFormula: expect.stringContaining(
+          `{${donorPaymentsFields.code}} = "some test code"`
+        ),
+      });
+      expect(result).toEqual(["some test record", null]);
+    });
+  });
+
+  describe("given a code that does not correspond to an existing donor payment record", () => {
+    beforeEach(() => {
+      mockSelect.mockReturnValue({ firstPage: () => undefined });
+    });
+
+    it("returns an error message", async () => {
+      const result = await findDonorPaymentByCode("some test code");
+
+      expect(mockSelect).toHaveBeenCalledWith({
+        filterByFormula: expect.stringContaining(
+          `{${donorPaymentsFields.code}} = "some test code"`
+        ),
+      });
+      expect(result).toEqual([null, "Valid payment with that code not found"]);
+    });
+  });
+});
