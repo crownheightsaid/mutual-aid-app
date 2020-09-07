7cb3b8ba19ab1b31975da11bf34610112c278c2b
John Sandfort <john@jsand.us>
2020-09-07 17:36:33 -0400

    Add negative test for delete request

--- src/lib/airtable/tables/requests.test.js
+++ src/lib/airtable/tables/requests.test.js
@@ -10,17 +10,34 @@ const { deleteRequest } = require('./requests.js');
 
 describe('deleteRequest', () => {
   describe('given an existing ID', () => {
-		test('sends a delete request to Airtable', async () => {
+    test('it sends a delete request to Airtable', async () => {
       mockDestroyFn.mockResolvedValue([{id: 'some-arbitrary-id'}]);
-
+      
       const id = 'some-arbitrary-id';
-
+      
       const result = await deleteRequest(id);
       
       expect(mockDestroyFn).toHaveBeenCalledWith([id]);
-
+      
       expect(result[0].id).toEqual(id);
       expect(result[1]).toEqual(null);
-		});
+    });
+  });
+
+  describe('given a nonexistent ID', () => {
+    beforeEach(() => {
+      mockDestroyFn.mockRejectedValue('an-error');      
+    });
+	  
+    test('it returns an error', async () => {
+      const id = 'another-id';
+
+      const result = await deleteRequest(id);
+
+      expect(mockDestroyFn).toHaveBeenCalledWith([id]);
+
+      expect(result[0]).toEqual(null);
+      expect(result[1]).toEqual('an-error');
+    });
   });
 });
