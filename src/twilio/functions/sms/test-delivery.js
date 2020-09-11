3a776eaecaef19f47ee8ea1872525ffc61395f29
MAB <queenMab@MABpro2017.local>
2020-09-10 22:16:04 -0400

    fix undefined values for twilio SMS messages

--- src/twilio/functions/sms/test-delivery.js
+++ src/twilio/functions/sms/test-delivery.js
@@ -8,7 +8,9 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
   const base = Airtable.base("apppPfEXed7SRcKmB"); // staging base
   let code = event.body.requestCode;
   const deliveryPhone = event.body.phoneNumber;
-  const deliveryName = "MAB TEST"; // get real delivery vol name from request
+  let deliveryName = "";
+  let missinginfo = false;
+  let missingintake = false;
   code = code.toUpperCase().trim();
 
   console.log("incoming message received");
@@ -25,25 +27,41 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
         console.log("Retrieved", record.get("Code"));
         console.log("ID", record.id);
         const phone = record.get("Phone");
-        const firstName = record.get("First Name");
+        let firstName = record.get("First Name");
+        if (typeof firstName === "undefined") {
+          firstName = "your neighbor";
+        }
         const list = record.get("Intake General Notes");
+        if (typeof list === "undefined") {
+          missinginfo = true;
+        }
         const street1 = record.get("Cross Street #1");
+        if (typeof street1 === "undefined") {
+          missinginfo = true;
+        }
         const street2 = record.get("Cross Street #2");
+        if (typeof street2 === "undefined") {
+          missinginfo = true;
+        }
         const volrecord = record.get("Intake volunteer");
 
-        // below: changes request status to "Delivery Assigned"
-        // react/express now handles assigning the delivery
-        /* base("Requests").update(
-          record.id,
-          {
-            Status: "Delivery Assigned",
-          },
-          function reAssign(reerror) {
-            if (reerror) {
-              console.error(reerror);
-            }
-          }
-        ); */
+        // looks for delivery volunteer name based on phone number
+        base("Volunteers")
+          .select({
+            view: "Grid view", // from staging base
+            maxRecords: 1,
+            filterByFormula: `({volunteer_phone} = '${deliveryPhone}')`,
+          })
+          .firstPage(function deliveryNameSeek(derror, dnamerecords) {
+            dnamerecords.forEach(function getName(dnamerecord) {
+              console.log("Retrieved", dnamerecord.get("volunter_name"));
+              console.log("ID", dnamerecord.id);
+              deliveryName = dnamerecord.get("volunteer_name");
+              if (typeof deliveryName === "undefined") {
+                deliveryName = "a CHMA volunteer";
+              }
+            });
+          });
 
         // below, uses linked 'Intake volunteer' record id to get phone number and name from Volunteers table
         base("Volunteers").find(volrecord, function checkVolunteer(volerror, vrecord) { // eslint-disable-line
@@ -53,16 +71,34 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
           }
           console.log("Retrieved", vrecord.id);
           const intakephone = vrecord.get("volunteer_phone");
-          const intakename = vrecord.get("volunteer_name");
+          let intakename = vrecord.get("volunteer_name");
+
+          if (typeof intakephone === "undefined") {
+            missingintake = true;
+          }
+
+          if (typeof intakename === "undefined") {
+            intakename = "ready to help";
+          }
+
+          let deliveryText = `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nTheir phone is ${phone}, you will need to get in touch with them about the full address. Their cross streets are ${street1} & ${street2}.\n\n${firstName}'s grocery list is: ${list}\n\nThe intake volunteer for this request is ${intakename}. Their phone # is ${intakephone}, and they can help if you have any questions - they'll reach out to you to follow up and make sure the delivery goes well!`;
+
+          if (missinginfo === true) {
+            deliveryText = `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nIt looks like some important info might be missing - please follow up with #wg_tech on Slack, or text your intake volunteer ${intakename} at ${intakephone} - we'll get it sorted out!`;
+          }
+
+          if (missingintake === true) {
+            deliveryText = `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nIt looks like some important info might be missing - please follow up with #intake_volunteers on Slack - we'll get it sorted out`;
+          }
 
           // sends SMS out to delivery volunteer
           const twilioClient = context.getTwilioClient();
 
           twilioClient.messages
             .create({
-              to: deliveryPhone, // need delivery volunteer phone number here
+              to: deliveryPhone,
               from: "+19175403381", // twilio number here
-              body: `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nTheir phone is ${phone}, you will need to get in touch with them about the full address. Their cross streets are ${street1} & ${street2}.\n\n${firstName}'s grocery list is: ${list}\n\nThe intake volunteer for this request is ${intakename}. Their phone # is ${intakephone}, and they can help if you have any questions - they'll reach out to you to follow up and make sure the delivery goes well!`,
+              body: deliveryText,
             })
             .then(function txtIntake() {
               twilioClient.messages.create(
