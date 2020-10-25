6ac668d40aa51e55010c87acbf7d2eeae8f7839b
John Sandfort <john@jsand.us>
2020-10-25 16:37:05 -0400

    more tests for findVolunteerByEmail

--- src/lib/airtable/tables/__tests__/volunteers.test.js
+++ src/lib/airtable/tables/__tests__/volunteers.test.js
@@ -11,7 +11,7 @@ const {
 } = require("../volunteers.js");
 
 describe("findVolunteerByEmail", () => {
-  describe("when the email is not found", () => {
+  describe("when the email is NOT found", () => {
     const email = "notreal@example.com";
     let result;
     
@@ -25,6 +25,38 @@ describe("findVolunteerByEmail", () => {
     test("it returns an error message", () => {
       expect(result[0]).toBeNull();
       expect(result[1]).toEqual(expect.stringContaining(`No volunteer signed up with email ${email}`));
+    });
+  });
+
+  describe("when the email IS found", () => {
+    let result;
+
+    beforeEach(async () => {
+      mockSelectFn.mockReturnValue({firstPage: () => ["a volunteer"]});
+
+      result = await findVolunteerByEmail("real@example.com");
+    });
+
+    test("it returns the volunteer record", () => {
+      expect(result[0]).toEqual("a volunteer");
+      expect(result[1]).toBeNull();
+    });
+  });
+
+  describe("when there is an error", () => {
+    const email = "error@example.com";
+    const errorMessage = "some error";
+    let result;
+
+    beforeEach(async () => {
+      mockSelectFn.mockReturnValue({firstPage: jest.fn().mockRejectedValue(errorMessage)});
+
+      result = await findVolunteerByEmail(email);
+    });
+
+    test("it returns the error message", () => {
+      expect(result[0]).toBeNull();
+      expect(result[1]).toEqual(`Errors looking up volunteer by email ${email}: ${errorMessage}`);
     });
   });
 });
