760250f7d5ea35a53a56cc1a3754ad377b6e8f41
John Sandfort <john@jsand.us>
2020-10-26 16:38:05 -0400

    Add test for findVolunteerByPhone

--- src/lib/airtable/tables/__tests__/volunteers.test.js
+++ src/lib/airtable/tables/__tests__/volunteers.test.js
@@ -11,7 +11,10 @@ jest.mock("~airtable/bases", () => ({
 const {
   findVolunteerByEmail,
   findVolunteerById,
+  findVolunteerByPhone,
 } = require("../volunteers.js");
+
+const { volunteersFields } = require("../volunteersSchema.js");
 
 describe("findVolunteerByEmail", () => {
   describe("when the email is NOT found", () => {
@@ -105,6 +108,70 @@ describe("findVolunteerByEmail", () => {
       test("it returns the error message", () => {
 	expect(result[0]).toBeNull();
 	expect(result[1]).toEqual(`Errors looking up volunteer by recordId ${id}: ${error}`);
+      });
+    });
+  });
+
+  describe("findVolunteerByPhone", () => {
+    describe("when the phone number IS found", () => {
+      let result;
+      const volunteer = "a volunteer";
+      const phone = "123-555-4444";
+      
+      beforeEach(async () => {
+	mockSelectFn.mockReturnValue({
+	  firstPage: () => [volunteer]
+	});
+
+	result = await findVolunteerByPhone(phone);
+      });
+      
+      test("it returns the volunteer", () => {
+	expect(result[0]).toEqual(volunteer);
+	expect(result[1]).toBeNull();
+      });
+
+      test("it makes a select request by phone number", () => {
+	expect(mockSelectFn).toHaveBeenCalledWith({
+	  maxRecords: 1,
+	  filterByFormula: `FIND("1235554444", ${volunteersFields.phone}) > 0`
+	});
+      });
+    });
+    describe("when the phone number is NOT found", () => {
+      let result;
+      const phone = "123-555-4444";
+
+      beforeEach(async () => {
+	mockSelectFn.mockReturnValue({
+	  firstPage: () => []
+	});
+	
+	result = await findVolunteerByPhone(phone);
+      });
+
+      test("it returns a 404 message", () => {
+	expect(result[0]).toBeNull();
+	expect(result[1]).toEqual(`404: No volunteer signed up with phone number ${phone}.`);
+      });
+    });
+    
+    describe("when there is an error in the request", () => {
+      let result;
+      const error = "an error";
+      const phone = "123-555-4444";
+      
+      beforeEach(async () => {
+	mockSelectFn.mockReturnValue({
+	  firstPage: jest.fn().mockRejectedValue(error)
+	});
+
+	result = await findVolunteerByPhone(phone);
+      });
+
+      test("it returns the error message", () => {
+	expect(result[0]).toBeNull();
+	expect(result[1]).toEqual(`Errors looking up volunteer by phone number ${phone}: ${error}`);
       });
     });
   });
