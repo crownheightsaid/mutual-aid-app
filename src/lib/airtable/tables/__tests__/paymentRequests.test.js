f0ca628ce80a75236909dcca411c56d58d5f746e
John Sandfort <john@jsand.us>
2020-10-27 16:13:35 -0400

    test API calls in updatePaymentRequestByCode

--- src/lib/airtable/tables/__tests__/paymentRequests.test.js
+++ src/lib/airtable/tables/__tests__/paymentRequests.test.js
@@ -22,24 +22,96 @@ const {
 
 describe("updatePaymentRequestByCode", () => {
   describe("when a payment request exists for the given code", () => {
-    let code, update, result;
+    let code, update, result, record;
     
     beforeAll(async () => {
       code = "123";
       update = { [paymentRequestsFields.balance]: "100" };
-
-      mockSelectFn.mockReturnValue({
-	firstPage: () => "a record"
+      
+      record = new Record(paymentRequestsTableName, "id-50", {
+	[paymentRequestsFields.amount]: "150",
+	[paymentRequestsFields.balance]: "150"
       });
+      
+      mockSelectFn.mockReturnValue({ firstPage: () => [record] });
 
       mockUpdateFn.mockReturnValue(["an updated record"]);
       
       result = await updatePaymentRequestByCode(code, update);
     });
+
+    test("it sends a select request", () => {
+      expect(mockSelectFn).toHaveBeenCalledWith({
+	filterByFormula: `(FIND('${code}', {${paymentRequestsFields.requestCode}}) > 0)`
+      });
+    });
+
+    test("it sends an update request with the new values", () => {
+      expect(mockUpdateFn).toHaveBeenCalledWith([{
+	id: record.id,
+	fields: {
+	  [paymentRequestsFields.balance]: "100"
+	}
+      }]);
+    });
     
     test("it returns the payment request record", () => {
       expect(result[0]).toEqual("an updated record");
       expect(result[1]).toBeNull();
+    });
+  });
+
+  describe("when no payment request exists for the given code", () => {
+    let result;
+    const code = "some code";
+    
+    beforeAll(async () => {
+      mockSelectFn.mockReturnValue({ firstPage: () => [] });
+
+      result = await updatePaymentRequestByCode(code, "some update");
+    });
+
+    test("it returns an error message", () => {
+      expect(result[0]).toBeNull();
+      expect(result[1]).toEqual(`No requests found with code: ${code}`);
+    });
+  });
+
+  describe("when an error occurs in the select request", () => {
+    let result;
+    const error = "a select error message";
+
+    beforeAll(async () => {
+      mockSelectFn.mockReturnValue({
+	firstPage: jest.fn().mockRejectedValue(error)
+      });
+
+      result = await updatePaymentRequestByCode("some code", "some update");
+    });
+    
+    test("it returns the error message", () => {
+      expect(result[0]).toBeNull();
+      expect(result[1]).toEqual(`Error while processing update: ${error}`);
+    });
+  });
+
+  describe("when an error occurs in the update request", () => {
+    let result;
+    const error = "an update error message";
+
+    beforeAll(async () => {
+      mockSelectFn.mockReturnValue({
+	firstPage: () => [new Record(paymentRequestsTableName, "some-id")]
+      });
+
+      mockUpdateFn.mockRejectedValue(error);
+
+      result = await updatePaymentRequestByCode("some code", "some update");
+    });
+
+    test("it returns the error message", () => {
+      expect(result[0]).toBeNull();
+      expect(result[1]).toEqual(`Error while processing update: ${error}`);
     });
   });
 });
