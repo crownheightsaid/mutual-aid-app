52c47ad241349dcfd3ee41005a5980f705c52427
John Sandfort <john@jsand.us>
2020-10-27 15:39:11 -0400

    test delete and start testing update

--- src/lib/airtable/tables/__tests__/paymentRequests.test.js
+++ src/lib/airtable/tables/__tests__/paymentRequests.test.js
@@ -1,7 +1,11 @@
+const mockSelectFn = jest.fn();
+const mockUpdateFn = jest.fn();
 const mockDestroyFn = jest.fn();
 
 jest.mock("~airtable/bases", () => ({
   paymentsAirbase: () => ({
+    select: mockSelectFn,
+    update: mockUpdateFn,
     destroy: mockDestroyFn,
   }),
 }));
@@ -9,19 +13,66 @@ jest.mock("~airtable/bases", () => ({
 const { Record } = require("airtable");
 const {
   deletePaymentRequest,
-  paymentRequestsTableName,
+  updatePaymentRequestByCode,
 } = require("../paymentRequests");
+const {
+  paymentRequestsTableName,
+  paymentRequestsFields,
+} = require("../paymentRequestsSchema");
+
+describe("updatePaymentRequestByCode", () => {
+  describe("when a payment request exists for the given code", () => {
+    let code, update, result;
+    
+    beforeAll(async () => {
+      code = "123";
+      update = { [paymentRequestsFields.balance]: "100" };
+
+      mockSelectFn.mockReturnValue({
+	firstPage: () => "a record"
+      });
+
+      mockUpdateFn.mockReturnValue(["an updated record"]);
+      
+      result = await updatePaymentRequestByCode(code, update);
+    });
+    
+    test("it returns the payment request record", () => {
+      expect(result[0]).toEqual("an updated record");
+      expect(result[1]).toBeNull();
+    });
+  });
+});
 
 describe("deletePaymentRequest", () => {
   let paymentRequestRecord;
 
-  beforeEach(() => {
+  beforeAll(async () => {
     paymentRequestRecord = new Record(paymentRequestsTableName, "some-id");
+    await deletePaymentRequest(paymentRequestRecord);
   });
 
-  test("sends a delete request to the Payment Request table", async () => {
-    await deletePaymentRequest(paymentRequestRecord);
-
+  test("it sends a delete request to the Payment Request table", () => {
     expect(mockDestroyFn).toHaveBeenCalledWith(paymentRequestRecord.id);
+  });
+
+  describe("when an error occurs", () => {
+    const error = "an error";
+    let consoleErrorFn;
+    
+    beforeAll(async () => {
+      consoleErrorFn = console.error;
+      console.error = jest.fn();
+      mockDestroyFn.mockRejectedValue(error);
+      await deletePaymentRequest(paymentRequestRecord);
+    });
+
+    afterAll(() => {
+      console.error = consoleErrorFn;
+    });
+
+    test("it logs the error", () => {
+      expect(console.error).toHaveBeenCalledWith(`Error while deleting payment request ${error}`);
+    });
   });
 });
