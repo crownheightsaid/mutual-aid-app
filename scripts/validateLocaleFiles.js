4c72ab7d46e98466bd72b0d6b0f12889a61d08ad
John Sandfort <john@jsand.us>
2020-11-25 12:46:13 -0500

    add json validation script

---
+++ scripts/validateLocaleFiles.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+
+function isValid(str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return e;
+  }
+}
+
+function validateFile(filePath) {
+  const data = fs.readFileSync(filePath, 'utf8');
+  const error = isValid(data);
+
+  if (error) {
+    console.error("Error in file: ", filePath);
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+const localesPath = "./src/lib/strings/locales";
+
+const dirs = fs.readdirSync(localesPath);
+dirs.forEach(dir => {
+  const files = fs.readdirSync(path.join(localesPath, dir));
+  files.forEach(file => {
+    validateFile(path.join(localesPath, dir, file));
+  });
+});
